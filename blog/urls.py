6b2d22460c7480e887eceb8cdb3437b331777c6c
joohyeong <joohyenog2001@gmail.com>
2020-02-06 14:37:22 +0900

    new apps - home,user 20200206.14.0

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('user/', include('user.urls')),
+    path('', RedirectView.as_view(url='/home/'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
