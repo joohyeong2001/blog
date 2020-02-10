f05df49beb22eac366a324c6a6ed97fff962daf0
joohyeong <joohyenog2001@gmail.com>
2020-02-10 21:15:21 +0900

    created posts 20200210.21.0

--- blog/settings.py
+++ blog/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig',
-    'user.apps.UserConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
--- blog/urls.py
+++ blog/urls.py
@@ -23,9 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
-    path('user/', include('user.urls')),
-    path('', RedirectView.as_view(url='/home/'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
