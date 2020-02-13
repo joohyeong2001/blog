27bbf1e86cafebd9eaac5fc764e4abb166fc314e
JunhyunJunhyun <sji010900@gmail.com>
2020-02-13 13:21:59 +0900

    Stable Version 20200213.13.0

--- posts/models.py
+++ posts/models.py
@@ -7,8 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     category = models.TextField(default="일상")
-    tag = models.TextField(default="블로그 챌린지")
-    post_views = models.IntegerField(default=0)
     # modified_date = models.DateTimeField(auto_add=True)
 
     def __str__(self):
