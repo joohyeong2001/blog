0b17969e50ed86214dbd913de8bc04e62b6cc1c6
JunhyunJunhyun <sji010900@gmail.com>
2020-02-13 10:31:25 +0900

    Stable Version 20200213.10.0

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    category = models.TextField()
     # modified_date = models.DateTimeField(auto_add=True)
