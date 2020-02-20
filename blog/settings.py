377e5e464804d8ca8e91fa73941143b70f7690ba
joohyeong <joohyenog2001@gmail.com>
2020-02-20 12:26:49 +0900

    20200220.12.0

--- blog/settings.py
+++ blog/settings.py
@@ -119,6 +119,7 @@ LOGIN_REDIRECT_URL = "/"
 
 
 # Media files
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views import debug
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 
@@ -9,3 +10,4 @@ urlpatterns = [
     path('postlist/', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
