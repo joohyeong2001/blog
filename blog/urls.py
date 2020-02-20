f2ff1c4a67e440f3821116d1a9fb08f5e794bcda
JunhyunJunhyun <sji010900@gmail.com>
2020-02-20 12:34:28 +0900

    이미지 처리 기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -24,9 +24,10 @@ from django.conf.urls.static import static
 from posts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("posts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from django.views import debug
-from django.conf.urls.static import static
-from django.conf import settings
+
 from . import views
 
 
 urlpatterns = [
-    path('', views.Index, name='index'),
-    path('postlist/', views.PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path("", views.Index, name="index"),
+    path("postlist/", views.PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>", views.PostDetailView.as_view(), name="post-detail"),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
