a0dc9e9f1b61bca95d507524d9a38891eca2d11f
JunhyunJunhyun <sji010900@gmail.com>
2020-02-26 11:51:02 +0900

    CBV to FBV Start

--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse
-from .models import Post
-
 from django.http import HttpResponseRedirect
+
+from .models import Post
 
 
 def Index(request):
     post = Post.objects.all()
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "post": post,
-        'num_visits': num_visits,
+        "num_visits": num_visits,
     }
 
     return render(request, "index.html", context=context)
-    
 
-class PostListView(generic.ListView):
-    model = Post
-    paginate_by = 5
+
+# CBV PostListView
+# class PostListView(generic.ListView):
+#     model = Post
+#     paginate_by = 5
+
+# FBV - PostListView
 
 
 class PostDetailView(generic.DetailView):
