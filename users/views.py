6036ebdcbfba94bfbc1aac81760b977e158249f2
JunhyunJunhyun <sji010900@gmail.com>
2020-02-26 11:44:18 +0900

    코드 첨삭

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 from .models import AbsBaseUser
 
-from django.http import HttpResponseRedirect
 
 class UserListView(generic.ListView):
     model = AbsBaseUser
