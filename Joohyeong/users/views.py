190b25d1effdc04eebb2e690881f1d8bcc86136b
joohyeong <joohyenog2001@gmail.com>
2020-02-27 11:48:30 +0900

    UnicodeDecodeError 20200227.11.0

--- Joohyeong/users/views.py
+++ Joohyeong/users/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 
 from .models import User
 
 
-class UserListView(generic.ListView):
-    model = User
-    paginate_by = 5
+# class UserListView(generic.ListView):
+#     model = User
+#     paginate_by = 5
+
+def UserListView(request):
+    users = User.objects.all()
+
+    context = {"users": users}
+
+    return render(request, "user_list.html", context=context)
 
 
-class UserDetailView(generic.DetailView):
-    model = User
+# class UserDetailView(generic.DetailView):
+#     model = User
+
+def UserDetailView(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except user.DoesNotExist:
+        raise Http404("user %s does not exist" % pk)
+
+    return render(request, "user_detail.html", {"user": user})
 
 
 def UserProfile(request):
